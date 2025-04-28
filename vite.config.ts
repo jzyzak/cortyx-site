85676f1b4221d80ec00402b34ed04d2d5cb44872
jzyzak <joshuazyzak@gmail.com>
2025-04-27 21:39:14 -0400

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// detect if we are building for production
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
+  base: isProd ? '/cortyx-site/' : '/',
   plugins: [react()],
-  base: '/cortyx-site/',
 });
