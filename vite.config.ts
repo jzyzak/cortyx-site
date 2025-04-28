7087bba34286ecaa36ff63714b2217cf1c0ab873
jzyzak <joshuazyzak@gmail.com>
2025-04-28 03:32:37 -0400

    fix for domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';  // IMPORTANT
 
 export default defineConfig({
-  base: '/cortyx-site/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 });
