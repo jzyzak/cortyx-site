d5486b9dabb77c84e5382cd21074c677726e0828
jzyzak <joshuazyzak@gmail.com>
2025-04-27 21:42:35 -0400

    please work fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';  // <-- use HashRouter
+import App from './App';
+import './index.css';
 
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
